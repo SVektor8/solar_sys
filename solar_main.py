6cdec03c7d9038c84f07cda92a02cbbfb9c3c43b
SVektor8 <shilov.vn@phystech.edu>
2021-11-15 20:42:54 +0300

    Cosmetic and PEP-8 changes; moved read and write space objects data from file functions from solar_input.py to solar_main.py; deleted solar_input.py and solar_model.py because they became blank

--- solar_main.py
+++ solar_main.py
@@ -1,11 +1,8 @@
 # coding: utf-8
 # license: GPLv3
 
-import pygame as pg
 from solar_vis import *
-from solar_model import *
-from solar_input import *
-from solar_objects import *
+from solar_objects import SpaceObject
 import thorpy
 import time
 import numpy as np
@@ -21,12 +18,57 @@ model_time = 0
 """Физическое время от начала расчёта.
 Тип: float"""
 
-time_scale = 1000.0
+time_scale = 1000000.0
 """Шаг по времени при моделировании.
 Тип: float"""
 
 space_objects = []
 """Список космических объектов."""
+
+
+def read_space_objects_data_from_file(input_filename):
+    """Cчитывает данные о космических объектах из файла, создаёт сами объекты
+    и вызывает создание их графических образов
+
+    Параметры:
+
+    **input_filename** — имя входного файла
+    """
+
+    objects = []
+    with open(input_filename, 'r') as input_file:
+        for line in input_file:
+            if len(line.strip()) == 0 or line[0] == '#':
+                continue  # пустые строки и строки-комментарии пропускаем
+
+            object_type = line.split()[0].lower()
+            if object_type == "star" or object_type == "planet":
+                object = SpaceObject(line)
+                objects.append(object)
+            else:
+                print("Unknown space object")
+    return objects
+
+
+def write_space_objects_data_to_file(output_filename, space_objects):
+    """Сохраняет данные о космических объектах в файл.
+
+    Строки должны иметь следующий формат:
+
+    Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+
+    Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+
+    Параметры:
+
+    **output_filename** — имя входного файла
+
+    **space_objects** — список объектов планет и звёзд
+    """
+    with open(output_filename, 'w') as out_file:
+        for obj in space_objects:
+            print(out_file, str(obj))
+            # FIXED! use __str__ of space objects - сейчас может быть использовано
 
 
 def execution(delta):
@@ -37,7 +79,10 @@ def execution(delta):
     """
     global model_time
     global displayed_time
-    recalculate_space_objects_positions([dr.obj for dr in space_objects], delta)
+
+    for obj in space_objects:
+        obj.move(space_objects, delta)
+
     model_time += delta
 
 
@@ -74,7 +119,7 @@ def open_file():
     model_time = 0.0
     in_filename = "solar_system.txt"
     space_objects = read_space_objects_data_from_file(in_filename)
-    max_distance = max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in space_objects])
+    max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in space_objects])
     calculate_scale_factor(max_distance)
 
 
@@ -150,7 +195,7 @@ def main():
     pg.init()
 
     width = 1000
-    height = 900
+    height = 600
     screen = pg.display.set_mode((width, height))
     last_time = time.perf_counter()
     drawer = Drawer(screen)
--- solar_objects.py
+++ solar_objects.py
@@ -1,10 +1,13 @@
 # coding: utf-8
 # license: GPLv3
+
 from typing import List
-from solar_vis import DrawableObject
+import pygame as pg
+from solar_vis import scale_y, scale_x
 
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
+
 
 class SpaceObject:
 
@@ -16,42 +19,54 @@ class SpaceObject:
         Object <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
         """
         parsed = line.lower().split()
-        self.type  = parsed[0]
-        self.R     = float(parsed[1])
+
+        self.type = parsed[0]
+        self.R = float(parsed[1])
         self.color = parsed[2]
-        self.m     = float(parsed[3])
-        self.x     = float(parsed[4])
-        self.y     = float(parsed[5])
-        self.Vx    = float(parsed[6])
-        self.Vy    = float(parsed[7])
+        self.m = float(parsed[3])
+        self.x = float(parsed[4])
+        self.y = float(parsed[5])
+        self.Vx = float(parsed[6])
+        self.Vy = float(parsed[7])
 
     def __str__(self):
-        '''Возвращает строку данных об объекте.'''
-        SEP = ' '
-        line = self.type + SEP + str(self.R) + SEP + self.color + SEP + str(self.m) + SEP \
-               + str(self.x) + SEP + str(self.y) + SEP + str(self.Vx) + SEP + str(self.Vy)
+        """Возвращает строку данных об объекте."""
+        sep = ' '
+        parameters = [self.type, str(self.R), self.color, str(self.m),
+                      str(self.x), str(self.y), str(self.Vx), str(self.Vy)]
+        line = sep.join(parameters)
         return line
 
     def move(self, space_objects, dt=1):
         Fx = Fy = 0
-        x_sign = 1   # направление оси X у программы (1 - если вправо, -1 - если влево)
-        y_sign = -1   # направление оси Y у программы (1 - если вверх, -1 - если вниз)
+        x_sign = 1  # направление оси X у программы (1 - если вправо, -1 - если влево)
+        y_sign = -1  # направление оси Y у программы (1 - если вверх, -1 - если вниз)
 
         for obj in space_objects:
             if self == obj:
                 continue
-            r = ((self.x - obj.x)**2 + (self.y - obj.y)**2)**0.5\
-
+            r = ((self.x - obj.x) ** 2 + (self.y - obj.y) ** 2) ** 0.5 \
+ \
             # FIXED вариант небольшого фикса обработки аномалий при прохождении
             # планет друг через друга
             r = max(r, self.R + obj.R)
             x, y = obj.x - self.x, obj.y - self.y  # векторы расстояний от тела до другого объекта
-            force = gravitational_constant * self.m * obj.m / r**2
+            force = gravitational_constant * self.m * obj.m / r ** 2
             Fx += x_sign * force * x / r
-            Fy += y_sign * force * y / r 
+            Fy += y_sign * force * y / r
         ax, ay = Fx / self.m, Fy / self.m
-        self.x += self.Vx*dt + 0.5*ax*dt**2
-        self.y += self.Vy*dt + 0.5*ay*dt**2
-        self.Vx += ax*dt
-        self.Vy += ay*dt
+        self.x += self.Vx * dt + 0.5 * ax * dt ** 2
+        self.y += self.Vy * dt + 0.5 * ay * dt ** 2
+        self.Vx += ax * dt
+        self.Vy += ay * dt
         # FIXED write simple move func like solar_model.py move_space_object()
+        # FIXME планеты почему-то двигаются не вокруг Солнца, а по другим траекториям
+
+    def draw(self, surface):
+        if self.type == "star":
+            pg.draw.circle(surface, (255, 0, 0),
+                           (scale_x(self.x), scale_y(self.y)), self.R)
+            print(scale_x(self.x), scale_y(self.y))
+        else:
+            pg.draw.circle(surface, (255, 255, 255),
+                           (scale_x(self.x), scale_y(self.y)), self.R)
